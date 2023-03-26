9296cf73f0eab4c8565fefefff6d1b32129a3d7d
Tina Dessu <tinady2009@gmail.com>
2023-03-26 22:24:28 +0300

    Update App.js

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -4,9 +4,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
      <p>Tina</p>
+     <Info/>
+     <Additem/>
+     <Additem/>
+     <Additem/>
+     
     </div>
   );
 }
+
+function Info(){
+  return(
+    <div>
+      <h1>Inventory system</h1>
+      <p>Manage your stuff.</p>
+    </div>
+  );
+}
+
+function Additem(){
+  return(
+    <div>
+      <form>
+        <label for="text-form"> type something </label>
+        <input type="text" id="text-form"/>
+        </form>
+      
+    </div>
+  )
+}
+
 
 export default App;
