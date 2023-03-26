af2b8840c2fafbbab89b5f667893d256812f80f3
Tina Dessu <tinady2009@gmail.com>
2023-03-26 23:32:26 +0300

    Update App.js

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -10,15 +10,19 @@ function App() {
      <Additem/>
      <Additem/>
      <Additem/>
-     <Additem/>
+     
 
     </div>
   );
 }
 
 function Info(){
+  const showtitle=true;
+  const Title="this is my title";
+
   return(
     <div>
+      <h1>{showtitle? Title: "no title"}</h1>
       <h1>Inventory system</h1>
       <p>Manage your stuff.</p>
     </div>
